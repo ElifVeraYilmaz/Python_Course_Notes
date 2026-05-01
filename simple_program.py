953c7d7e00eb7c197a6094a3dd885e0a9c1a556d
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-01 21:15:53 +0300

    variables learned

--- simple_program.py
+++ simple_program.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("Hello World") #Single or double quotes can be used inside.
+print('0-----')
+print(' |||||')
+print ('*' * 10) 
+# We multiply the string * by 10. 10 * will be printed. The value inside the parentheses is an expression.
+
+########## VARIABLES ##########
+
+price = 10 #integer
+price = 20 # It will print 20 as a variable. Because Python reads line by line.
+
+rating = 4.9 #float
+name = 'Mosh' #string
+is_published = False #boolean
+
+print (price)
