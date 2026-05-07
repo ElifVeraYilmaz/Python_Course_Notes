9f6af2ceb9d2d242697de70a26887ed7cadeca3e
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-07 16:14:02 +0300

    feat: implement conditional statements and logical operators.

---
+++ 06_If_Else_LogicalOp.py
@@ -0,0 +1,35 @@
+######## If/Else ##########
+is_hot = False 
+is_cold = False 
+
+if is_hot :
+     print("Its a hot day.")
+elif is_cold :
+     print("Its a cold day.")
+else:
+     print("Its a lovely day.")
+
+print("Have a nice day.")
+
+
+
+######## Logical Operations ##########
+
+
+has_high_income = True 
+has_criminal_record = False
+
+if has_high_income and not has_criminal_record:
+     print("Eligable for loan.")
+else:
+     print("Not eligable for loan.")
+
+################
+
+temperature = 30 
+if temperature > 30:
+     print("Its a hot day.")
+else:
+     print("Its not a hot day.")
+
+     
