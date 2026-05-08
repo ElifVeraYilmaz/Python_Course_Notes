066ddb8d89f287611b9f31bd46b554505217e23a
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-09 02:07:39 +0300

    feat: implement for loops over strings, lists, and range sequences

---
+++ 11_For_Loop.py
@@ -0,0 +1,18 @@
+for item in 'Python': # The loop goes through the word 'Python' letter by letter.
+     print(item)
+
+for name in ['Mosh','John','Sarah']: #List
+     print(name)
+
+for number in [1,2,3,4]:
+     print(number)
+
+for number in range(10):
+     print(number)
+
+for number in range(5,10): # 5,6,7,8,9 (5. the number is not included, but 10. the number is included)
+     print(number)
+
+for number in range(5, 10, 2): # 5,7,9
+     print(number)
+     
