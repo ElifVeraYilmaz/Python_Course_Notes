5bed2408b4973bcda5281feb5d879bf130faddcd
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-09 14:48:02 +0300

    docs: demonstrate tuple methods and unpacking syntax

---
+++ 19_Tuple_Unpacking.py
@@ -0,0 +1,17 @@
+######## Tuple ##########
+
+numbers = (1, 2, 3) #Tuple 
+print(numbers[0])
+# We cannot change the list.
+
+######## Unpacking ##########
+
+coordinates = (1, 2, 3)
+
+''' x = coordinates [0]
+    y = coordinates [1]
+    z = coordinates [2] -> This is long way.'''
+x, y, z = coordinates 
+
+print(y)
+
