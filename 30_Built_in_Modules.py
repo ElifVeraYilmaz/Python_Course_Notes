3bf707409c6aff2fa4c9cc9852d4f3d42449473b
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-10 00:07:55 +0300

    feat: complete python basics course and refine code documentation

--- 30_Built_in_Modules.py
+++ 30_Built_in_Modules.py
@@ -1,13 +1,17 @@
-# seach 'Python 3 module ibdex' on google.
+# Search the 'Python 3 module index' on Google.
 
 import random
 
 for i in range(3):
-  print(random.random()) # When we call it will give us random number 0 to 1
+  print(random.random()) # Generates a random float between 0 and 1.
   print(random.randint(10, 20))
 
 
 members = ['John', 'Mary']  
 leader = random.choice(members)
 print(leader)
+
+
+
+
 
