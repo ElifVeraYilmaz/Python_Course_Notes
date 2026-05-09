3207cd09bb48b5e3e86b16adcd744944d5e748ba
ElifVeraYilmaz <yilmazelifvera@gmail.com>
2026-05-09 13:47:47 +0300

    feat: implement algorithm to find the largest number in a list

---
+++ 14_List.py
@@ -0,0 +1,6 @@
+names = ['John','Bob','Mosh','Sarah', 'Mary']
+names[0] = 'Jon' # The first element has been updated.
+print(names[0])
+print(names [2:]) # Mosh , Sarah , Mary
+print(names [2:4]) # Mosh , Sarah
+
